0ad5422d0e1badf8a567cf2703a339de4e1c5e15
azizatif <azizatif@localhost>
2011-06-01 19:47:36 +0200

    Unused imports in MongoDbErrorLog

--- src/Elmah.MongoDb/MongoDbErrorLog.cs
+++ src/Elmah.MongoDb/MongoDbErrorLog.cs
@@ -3,18 +3,14 @@
     #region Imports
 
     using System;
-    using System.Collections.Generic;
-    using System.Data;
-    using System.Data.SqlClient;
-    using System.IO;
     using System.Linq;
-    using System.Text;
     using System.Threading;
-    using System.Xml;
-    using IDictionary = System.Collections.IDictionary;
     using MongoDB;
     using System.Configuration;
     using System.Collections;
+
+    using IDictionary = System.Collections.IDictionary;
+
     #endregion
 
     /// <summary>
