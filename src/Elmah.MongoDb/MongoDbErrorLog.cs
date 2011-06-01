34aae35cc9d57a24c3c1858f743a136718adfdc7
Atif Aziz <code@raboof.com>
2011-06-01 19:50:10 +0200

    License header

--- src/Elmah.MongoDb/MongoDbErrorLog.cs
+++ src/Elmah.MongoDb/MongoDbErrorLog.cs
@@ -1,4 +1,27 @@
-﻿namespace Elmah.MongoDb
+﻿#region License, Terms and Author(s)
+//
+// ELMAH Sandbox
+// Copyright (c) 2010-11 Atif Aziz. All rights reserved.
+//
+//  Author(s):
+//
+//      Pablo Cibraro
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+namespace Elmah.MongoDb
 {
     #region Imports
 
