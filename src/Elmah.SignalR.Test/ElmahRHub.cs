ae32f0419f4fb4fa35cc80a35389a7688de339b4
Roberto Vespa <roby@robychechi.it>
2012-02-03 22:46:51 +0100

    Removing unused public method on ElmahRHub.

--- src/Elmah.SignalR.Test/ElmahRHub.cs
+++ src/Elmah.SignalR.Test/ElmahRHub.cs
@@ -11,10 +11,5 @@
     [HubName("elmahr")]
     public class ElmahRHub : Hub
     {
-        public void Send(Error error)
-        {
-            // Call the 'notifyError' method on all clients
-            Clients.notifyError(error);
-        }
     }
 }
