76b2d4c2341348f4104ee6aacd009d6e3c2c1b7f
wasp <wasp@xps2wasp.dummy.porta.siemens.net>
2012-02-03 22:18:46 +0100

    Adding a basic SignalR sample which receives an Elmah error and sends it to any connected cclient.

---
+++ src/Elmah.SignalR.Test/ElmahRHub.cs
@@ -0,0 +1,20 @@
+﻿namespace Elmah.SignalR.Test
+{
+    #region Imports
+
+    using global::SignalR.Hubs;
+
+    #endregion
+
+    // Elmah-SignalR hub
+
+    [HubName("elmahr")]
+    public class ElmahRHub : Hub
+    {
+        public void Send(Error error)
+        {
+            // Call the 'notifyError' method on all clients
+            Clients.notifyError(error);
+        }
+    }
+}
