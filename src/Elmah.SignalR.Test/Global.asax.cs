f05fdfa462ee517f3184c7538ce8cc85ec83bd14
wasp <wasp@xps2wasp.dummy.porta.siemens.net>
2012-02-04 11:40:20 +0100

    Initializing ErrorsStore.

---
+++ src/Elmah.SignalR.Test/Global.asax.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Web;
+
+namespace Elmah.SignalR.Test
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            ErrorsStore.Store
+                .AddSource(
+                    "Wasp is doing ElmahR", 
+                    "The fool on the hill");
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
