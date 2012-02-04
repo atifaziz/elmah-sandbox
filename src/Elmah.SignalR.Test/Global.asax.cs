639845d3d64baa2fd15f63e69db6a45dab82a914
Roberto Vespa <roby@robychechi.it>
2012-02-04 21:26:39 +0100

    Adding ELMAH configuration to sample web site, making it a self-hosting application with both error source page and monitoring page. Makes it simpler to have a working sample and to check ELMAH configuration.

--- src/Elmah.SignalR.Test/Global.asax.cs
+++ src/Elmah.SignalR.Test/Global.asax.cs
@@ -12,38 +12,12 @@ namespace Elmah.SignalR.Test
                     "Wasp is doing ElmahR", 
                     "The fool on the hill")
                 .AddSource(
+                    "Wasp is doing ElmahR with self-hosting",
+                    "Lucy in the Sky with Diamonds")
+                .AddSource(
                     "Wasp is doing ElmahR again",
                     "Strawberry fields forever");
         }
 
-        protected void Session_Start(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Application_BeginRequest(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Application_AuthenticateRequest(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Application_Error(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Session_End(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Application_End(object sender, EventArgs e)
-        {
-
-        }
     }
 }
