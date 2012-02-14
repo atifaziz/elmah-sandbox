13346bafc618f82a373d5a99d6e11ea3c21ef59e
wasp <wasp@localhost>
2012-02-14 22:41:22 +0100

    Adding FakeLoadErrorPostModule.cs as a way to simulate multiple errors sources, for development purposes only.

---
+++ src/Sandbox/FakeLoadErrorPostModule.cs
@@ -0,0 +1,23 @@
+﻿namespace Elmah.Sandbox
+{
+    #region Imports
+
+    using System;
+
+    #endregion
+
+    public class FakeLoadErrorPostModule : ErrorPostModule
+    {
+        protected override string GetHandshakeToken()
+        {
+            // UNDOCUMENTED HACK (for testing purposes only!!): if thr handshakeToken
+            // contains a comma-separated list of tokens, the module will pick one
+            // of them randomly, to simulate different sources generating errors.
+
+            var tokens = base.GetHandshakeToken().Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries);
+            var random = new Random();
+
+            return tokens[random.Next(tokens.Length)];
+        }
+    }
+}
