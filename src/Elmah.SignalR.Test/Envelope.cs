f2a560e6244562d29b4ad567e460e33228236d78
wasp <wasp@localhost>
2012-02-14 23:16:25 +0100

    Fixing code to use available error id, instead of some hashing strategy, in order to uniquely identify an error.

--- src/Elmah.SignalR.Test/Envelope.cs
+++ src/Elmah.SignalR.Test/Envelope.cs
@@ -3,7 +3,7 @@ namespace Elmah.SignalR.Test
     public class Envelope
     {
         public int id;
-        public string applicationName, infoUrl;
+        public string applicationName, infoUrl, errorId;
         public Error error;
     }
 }
