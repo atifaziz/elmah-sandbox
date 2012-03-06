f38c8d12ecb7888129dace2d739677ab4dd8a11a
Roberto Vespa <roby@robychechi.it>
2012-03-06 20:41:35 +0100

    Removing useless clearTimeout call.

--- src/Elmah.SignalR.Test/Scripts/elmahr.core.js
+++ src/Elmah.SignalR.Test/Scripts/elmahr.core.js
@@ -67,9 +67,8 @@ function ApplicationViewModel(applicationName, infoUrl, doStats) {
             $("abbr.timeago", elem).timeago();
             if (!$(elem).hasClass("onReconnect")) {
                 $(elem).addClass("hilite");
-                var handle = window.setTimeout(function () {
+                window.setTimeout(function () {
                     $(elem).removeClass("hilite");
-                    window.clearTimeout(handle);
                 }, 15000);
             }
         }
