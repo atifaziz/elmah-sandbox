c93bf7d578133c1057048bd49bc2e8e12e3ee06d
jamesdriscoll <jamesdriscoll@btinternet.com>
2012-03-08 19:46:48 +0000

    Further tidying up of project name

--- src/Elmah.Mvc/Properties/AssemblyInfo.cs
+++ src/Elmah.Mvc/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Web;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Elmah.Mvc3")]
+[assembly: AssemblyTitle("Elmah.Mvc")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Elmah.Mvc3")]
+[assembly: AssemblyProduct("Elmah.Mvc")]
 [assembly: AssemblyCopyright("Copyright © Atif Aziz, James Driscoll 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
