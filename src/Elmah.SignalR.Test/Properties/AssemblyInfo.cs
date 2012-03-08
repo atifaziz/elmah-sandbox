ba41ea33971f1df8871628d04d62f2c711a42191
Atif Aziz <code@raboof.com>
2012-03-08 22:53:18 +0100

    Ascii-izing copyright symbol

--- src/Elmah.SignalR.Test/Properties/AssemblyInfo.cs
+++ src/Elmah.SignalR.Test/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Elmah.SignalR.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright \u00a9 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
