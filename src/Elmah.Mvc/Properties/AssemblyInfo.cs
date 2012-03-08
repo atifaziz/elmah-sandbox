32c356b7e918a99ecf2decc8de8f58d574155ea6
azizatif <azizatif@localhost>
2012-03-08 22:53:18 +0100

    Ascii-izing copyright symbol

--- src/Elmah.Mvc/Properties/AssemblyInfo.cs
+++ src/Elmah.Mvc/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Web;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Elmah.Mvc")]
-[assembly: AssemblyCopyright("Copyright © Atif Aziz, James Driscoll 2011")]
+[assembly: AssemblyCopyright("Copyright \u00a9 Atif Aziz, James Driscoll 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
