f026242b4391e52d2c45d95ff3287a5357eb320f
James Driscoll <jamesdriscoll@btinternet.com>
2012-03-08 07:36:00 +0000

    Updating pack cmd to be in line with the main ELMAH project's pack Also, updated version of NuGet.exe

--- pack.cmd
+++ pack.cmd
@@ -3,29 +3,64 @@
 setlocal
 pushd "%~dp0"
 
-echo.
-echo Building ELMAH-Sandbox
-echo.
+set hr=---------------------------------------------------------------------------
+REM set binzip=ELMAH-1.2-sp1-bin-x86.zip
+set nuget=pkg\Tools\nuget.exe
 
+:main
+call :clean ^
+ && call :buildsandbox ^
+ && call :autoupdate ^
+ && call :packall pkg\*.nuspec
+goto :EOF
+
+
+:buildsandbox
 call build
+goto :EOF
 
-IF ERRORLEVEL 1 GOTO :EOF
 
-echo.
+:clean
+call :rd bin && call :rd base && call :rd tmp
+goto :EOF
+
+:rd
+if exist %1 rd %1 /s /q
+if exist %1 exit /b 1
+goto :EOF
+
+:md
+if not exist %1 md %1
+goto :EOF
+
+:download
+setlocal
+echo %hr%
+echo Downloading %1...
+call tools\wgets http://elmah.googlecode.com/files/%1 %2 %3 %4 %5 %6 %7 %8 %9
+goto :EOF
+
+:unzip
+setlocal
+echo %hr%
+tools\7za x %*
+goto :EOF
+
+:autoupdate
+echo %hr%
 echo Making sure that NuGet.exe is up to date...
-echo.
+"%nuget%" update -self
+goto :EOF
 
-pkg\Tools\nuget.exe update
-
+:packall
+for /f %%F in ('dir /a-d /b "%1"') DO (
+CALL :pack pkg\%%F
 IF ERRORLEVEL 1 GOTO :EOF
-
-if NOT EXIST "NuGet-Packages" md "NuGet-Packages"
-
-for /f %%F in ('dir /a-d /b pkg\*.nuspec') DO CALL :PACKAGE pkg\%%F
+)
 GOTO :EOF
 
-:PACKAGE
-echo -------------------------------------------------------------------------------
+:pack
+echo %hr%
 echo Packaging %1
-pkg\Tools\nuget.exe pack "%1" -verbose -o "NuGet-Packages"
+call :md bin && "%nuget%" pack "%1" -verbose -output bin
 GOTO :EOF
