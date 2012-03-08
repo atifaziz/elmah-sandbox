ba9362b7b266abd9271e93ce5d793cdae0e582cc
James Driscoll <jamesdriscoll@btinternet.com>
2012-03-08 19:29:33 +0000

    Replaced httpModule with PreApplicationStartupMethod

---
+++ src/Elmah.Mvc3/ElmahMvcBootstrap.cs
@@ -0,0 +1,39 @@
+﻿namespace Elmah
+{
+    #region Imports
+    using System.Web.Mvc;
+    using System.Web.Routing;
+    #endregion
+
+    public class ElmahMvcBootstrap
+    {
+        public static void Initialize()
+        {
+            GlobalFilters.Filters.Add(new HandleErrorAttribute());
+            var constraints = new RouteValueDictionary(new { elmahSecurityConstraint = new ElmahSecurityConstraint() });
+            var namespaces = new RouteValueDictionary(new[] { "Elmah" });
+            var routeHandler = new MvcRouteHandler();
+
+            var elmahRoute = new Route("elmah/{resource}",
+                                       new RouteValueDictionary(
+                                           new
+                                           {
+                                               controller = "Elmah",
+                                               action = "Index",
+                                               resource = UrlParameter.Optional
+                                           }),
+                                       constraints,
+                                       namespaces,
+                                       routeHandler);
+
+            RouteTable.Routes.Insert(0, elmahRoute);
+            elmahRoute = new Route("elmah/detail/{resource}",
+                                   new RouteValueDictionary(
+                                       new { controller = "Elmah", action = "Detail", resource = UrlParameter.Optional }),
+                                   constraints,
+                                   namespaces,
+                                   routeHandler);
+            RouteTable.Routes.Insert(0, elmahRoute);
+        }
+    }
+}
