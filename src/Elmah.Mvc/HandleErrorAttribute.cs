b43a9d4bbf8b43eaf4bb26c6e6c03972dd5d62a8
azizatif <azizatif@localhost>
2012-06-14 22:45:23 +0200

    Avoiding HttpContext.Current in HandleErrorAttribute

--- src/Elmah.Mvc/HandleErrorAttribute.cs
+++ src/Elmah.Mvc/HandleErrorAttribute.cs
@@ -24,59 +24,53 @@
 
 namespace Elmah
 {
-    #region Imports
-
-    using System;
-    using System.Web;
     using System.Web.Mvc;
-    
-    #endregion
 
     public class HandleErrorAttribute : System.Web.Mvc.HandleErrorAttribute
     {
-        private static ErrorFilterConfiguration _config;
-
         public override void OnException(ExceptionContext context)
         {
             base.OnException(context);
-
-            if (!context.ExceptionHandled) // if unhandled, will be logged anyhow
+            if (!context.ExceptionHandled       // if unhandled, will be logged anyhow
+                || TryRaiseErrorSignal(context) // prefer signaling, if possible
+                || IsFiltered(context))         // filtered?
                 return;
 
-            var e = context.Exception;
-            var httpContext = context.HttpContext.ApplicationInstance.Context;
-            if (httpContext != null && 
-                (RaiseErrorSignal(e, httpContext) // prefer signaling, if possible
-                 || IsFiltered(e, httpContext))) // filtered?
-                return;
-
-            LogException(e, httpContext);
+            LogException(context);
         }
 
-        private static bool RaiseErrorSignal(Exception e, HttpContext context)
+        private static bool TryRaiseErrorSignal(ExceptionContext context)
         {
-            var signal = ErrorSignal.FromContext(context);
+            var httpContext = context.HttpContext.Impl();
+            if (httpContext == null)
+                return false;
+            var signal = ErrorSignal.FromContext(httpContext);
             if (signal == null)
                 return false;
-            signal.Raise(e, context);
+            signal.Raise(context.Exception, httpContext);
             return true;
         }
 
-        private static bool IsFiltered(Exception e, HttpContext context)
+        private static bool IsFiltered(ExceptionContext context)
         {
-            if (_config == null)
-            {
-                _config = context.GetSection("elmah/errorFilter") as ErrorFilterConfiguration 
-                          ?? new ErrorFilterConfiguration();
-            }
+            var config = context.HttpContext.GetSection("elmah/errorFilter") 
+                         as ErrorFilterConfiguration;
 
-            var testContext = new ErrorFilterModule.AssertionHelperContext(e, context);
-            return _config.Assertion.Test(testContext);
+            if (config == null)
+                return false;
+
+            var testContext = new ErrorFilterModule.AssertionHelperContext(
+                                      context.Exception,
+                                      context.HttpContext.Impl());
+
+            return config.Assertion.Test(testContext);
         }
 
-        private static void LogException(Exception e, HttpContext context)
+        private static void LogException(ExceptionContext context)
         {
-            ErrorLog.GetDefault(context).Log(new Error(e, context));
+            var httpContext = context.HttpContext.Impl();
+            var error = new Error(context.Exception, httpContext);
+            ErrorLog.GetDefault(httpContext).Log(error);
         }
     }
 }
